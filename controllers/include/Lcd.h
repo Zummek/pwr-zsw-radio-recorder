85acd3d37d9636cfc28ba78690c24f2f79b35031
marcinzzz <249033@student.pwr.edu.pl>
2021-11-19 22:35:59 +0100

    Add Lcd to main.cpp, prints frequency

--- controllers/include/Lcd.h
+++ controllers/include/Lcd.h
@@ -7,29 +7,22 @@
 #define LCD_COLUMNS 16 
 #define LCD_ROWS 2
 
-<<<<<<< HEAD
 #define LCD_FREQUENCY_COL 0
 #define LCD_FREQUENCY_ROW 0
 #define LCD_RDS_COL 0
 #define LCD_RDS_ROW 1
 
-=======
->>>>>>> Add Lcd class with simple interface
 class Lcd
 {
 public:
   static LiquidCrystal_I2C lcd;
 
   static void init();
-<<<<<<< HEAD
   static void displayFrequency(int freq);
   static void displayRDS(char *rds);
 
 private:
   static void _write(uint8_t col, uint8_t row, char *text);
-=======
-  static void write(uint8_t col, uint8_t row, char *text);
->>>>>>> Add Lcd class with simple interface
 };
 
 #endif

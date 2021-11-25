10801db5585580c586c877eff440e00536f5ee28
Adam Jdryka <adam.jedryka98@gmail.com>
2021-11-26 00:10:19 +0100

    Fix and update code appearance

--- controllers/src/userController/Equalizer.cpp
+++ controllers/src/userController/Equalizer.cpp
@@ -1,7 +1,7 @@
 #include "Equalizer.h"
 #include "Arduino.h"
 
-int *Equalizer::bandValues;
+int Equalizer::bandValues[EQUALIZER_BAUNDS_COUNT];
 
 void Equalizer::init()
 {
@@ -9,7 +9,6 @@ void Equalizer::init()
   pinMode(EQUALIZER_STROBE, OUTPUT);
 
   digitalWrite(EQUALIZER_RESET, LOW);
-  delay(1);
   digitalWrite(EQUALIZER_RESET, HIGH);
 }
 
@@ -17,12 +16,12 @@ void Equalizer::process()
 {
   digitalWrite(EQUALIZER_RESET, HIGH);
   digitalWrite(EQUALIZER_RESET, LOW);
- 
+
   for (int band = 0; band < 7; band++)
   {
     digitalWrite(EQUALIZER_STROBE, LOW);
-    delayMicroseconds(36);
-    Equalizer::bandValues[band] = analogRead(EQUALIZER_ANALOG);
+    delayMicroseconds(30);
+    bandValues[band] = analogRead(EQUALIZER_ANALOG);
     digitalWrite(EQUALIZER_STROBE, HIGH);
   }
 

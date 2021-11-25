59fb2c32421c8d7110cd8169e2259ed35a552efb
Adam Jdryka <adam.jedryka98@gmail.com>
2021-11-26 00:10:19 +0100

    Fix and update code appearance

--- controllers/include/Equalizer.h
+++ controllers/include/Equalizer.h
@@ -6,11 +6,12 @@
 #define EQUALIZER_STROBE 3
 #define EQUALIZER_RESET 4
 #define EQUALIZER_ANALOG A3
- 
+#define EQUALIZER_BAUNDS_COUNT 7
+
 class Equalizer
 {
 private:
-  static int *bandValues;
+  static int bandValues[EQUALIZER_BAUNDS_COUNT];
 public:
   static void init();
   static void process();
--- controllers/src/userController/Lcd.cpp
+++ controllers/src/userController/Lcd.cpp
@@ -1,5 +1,94 @@
 #include "Lcd.h"
+#include "Equalizer.h"
 #include "Arduino.h"
+
+byte char1[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+};
+
+byte char2[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+};
+
+byte char3[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte char4[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte char5[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte char6[8] = {
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte char7[8] = {
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte char8[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
 
 LiquidCrystal_I2C Lcd::lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
@@ -7,9 +96,17 @@ void Lcd::init()
 {
   lcd.init();
   lcd.backlight();
+  Lcd::lcd.createChar(0, char1);
+  Lcd::lcd.createChar(1, char2);
+  Lcd::lcd.createChar(2, char3);
+  Lcd::lcd.createChar(3, char4);
+  Lcd::lcd.createChar(4, char5);
+  Lcd::lcd.createChar(5, char6);
+  Lcd::lcd.createChar(6, char7);
+  Lcd::lcd.createChar(7, char8);
 }
 
-void Lcd::_write(uint8_t col, uint8_t row, char *text) {
+void Lcd::_write(uint8_t col, uint8_t row, char* text) {
   lcd.setCursor(col, row);
   lcd.print(text);
 }
@@ -25,12 +122,16 @@ void Lcd::displayFrequency(int freq) {
   Lcd::_write(LCD_FREQUENCY_COL, LCD_FREQUENCY_ROW, buffer);
 }
 
-void Lcd::displayRDS(char *rds) {
+void Lcd::displayRDS(char* rds) {
   Lcd::_write(LCD_FREQUENCY_COL, LCD_FREQUENCY_ROW, rds);
 }
 
-void Lcd::displayBands(int *values, uint8_t row) {
-  for (int i = 0; i < 7; i++) {
+void Lcd::displayBands(int values[], uint8_t row) {
+  Serial.print("Bands: ");
+  for (int i = 0; i < EQUALIZER_BAUNDS_COUNT; i++) {
+    Serial.print(values[i]);
+    Serial.print(" ");
     Lcd::_writeByte(i, row, map(values[i], 0, 1023, 0, 7));
   }
+  Serial.println();
 }

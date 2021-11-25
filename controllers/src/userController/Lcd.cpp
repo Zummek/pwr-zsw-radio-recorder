d2bbc8f1081ddfecfd6c4117abe0531a82a9e89e
marcinzzz <249033@student.pwr.edu.pl>
2021-11-25 18:38:52 +0100

    Add Equalizer, there is sneaky bug
    Add reading data from EQ
    Add displaying bands on LCD

--- controllers/src/userController/Lcd.cpp
+++ controllers/src/userController/Lcd.cpp
@@ -1,4 +1,5 @@
 #include "Lcd.h"
+#include "Arduino.h"
 
 LiquidCrystal_I2C Lcd::lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS);
 
@@ -13,6 +14,11 @@ void Lcd::_write(uint8_t col, uint8_t row, char *text) {
   lcd.print(text);
 }
 
+void Lcd::_writeByte(uint8_t col, uint8_t row, int n) {
+  lcd.setCursor(col, row);
+  lcd.write(n);
+}
+
 void Lcd::displayFrequency(int freq) {
   char buffer[11];
   sprintf(buffer, "%3d.%02d MHz", freq / 100, freq % 100);
@@ -21,4 +27,10 @@ void Lcd::displayFrequency(int freq) {
 
 void Lcd::displayRDS(char *rds) {
   Lcd::_write(LCD_FREQUENCY_COL, LCD_FREQUENCY_ROW, rds);
+}
+
+void Lcd::displayBands(int *values, uint8_t row) {
+  for (int i = 0; i < 7; i++) {
+    Lcd::_writeByte(i, row, map(values[i], 0, 1023, 0, 7));
+  }
 }
--- controllers/src/userController/main.cpp
+++ controllers/src/userController/main.cpp
@@ -4,6 +4,95 @@
 #include "AppRadio.h"
 #include "Controllers.h"
 #include "Lcd.h"
+#include "Equalizer.h"
+
+byte v1[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+};
+
+byte v2[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+};
+
+byte v3[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte v4[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte v5[8] = {
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte v6[8] = {
+  B00000,
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte v7[8] = {
+  B00000,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
+
+byte v8[8] = {
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+  B11111,
+};
 
 void setup()
 {
@@ -17,13 +106,27 @@ void setup()
   Controllers::init();
   AppRadio::init();
 
+  Equalizer::init();
   Lcd::init();
+  Lcd::lcd.createChar(0, v1);
+  Lcd::lcd.createChar(1, v2);
+  Lcd::lcd.createChar(2, v3);
+  Lcd::lcd.createChar(3, v4);
+  Lcd::lcd.createChar(4, v5);
+  Lcd::lcd.createChar(5, v6);
+  Lcd::lcd.createChar(6, v7);
+  Lcd::lcd.createChar(7, v8);
 }
+
+int i = 0; // to check if program is running
 
 void loop()
 {
   AppRadio::radio.checkRDS();
   Controllers::readAndProcess();
+
+  Equalizer::process();
+  Serial.println(i++);
 
   delay(50); // TODO: temporary delay to avoid overflow
 }
